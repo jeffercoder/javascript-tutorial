821fde1422c8c9ee242617b92891ea99cbd93b97
jeffercoder <johnday68@gmail.com>
2016-03-22 08:38:40 -0500

    Classes 032116

---
+++ app.js
@@ -0,0 +1,50 @@
+var greeting = "Hello, ";
+
+var name = "Johnny";
+
+var year = "2016";
+
+var yes = "true";
+
+var message = greeting + name;
+
+var greeting =
+  ["Hello, ",
+   "Bongour, ",
+   "Ohio, "
+];
+
+var student = {
+  first_name: 'Johnny',
+  last_name: 'Mejias',
+  age: 22
+};
+
+student['profession'] = 'developer'
+
+console.log(greetings[1], student.profession);
+
+var x = function(y) {
+  return y;
+}
+
+var full_name = function(first_name, last_name) {
+  return first_name + ' ' + last_name;
+}
+
+var myConsole = {
+log: function(value) {
+  return value;
+}
+}
+
+var username = function (email)
+
+
+//Date types for my system
+// string 'Johnny'
+// number  751
+// array   [1,'second']
+// object  { name: 'Johnny'}
+// boolean  true
+// function  function(params ....) { ... }
